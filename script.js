ebd05e3d40df168eab0740c41ee134cb7bb0ed7b
yarikyoucan <yaroslav.andrunyk@gmail.com>
2025-08-24 17:09:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ window.onload = function(){
   if (t50){
     if (task50Completed){ t50.innerText="Виконано"; t50.classList.add("done"); }
     t50.addEventListener("click", ()=>{
-      if (highscore >= 50 && !task50Completed){
+      if (highscore >= 75 && !task50Completed){
         addBalance(10);
         t50.innerText="Виконано"; t50.classList.add("done");
         task50Completed = true; saveData();
