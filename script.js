73ef58d7178465ed444fe84682fb5ade1c9fe154
yarikyoucan <yaroslav.andrunyk@gmail.com>
2025-10-15 19:32:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const POST_AD_TIMER_MS = 15_000;
 const GAMES_TARGET = 100;
 const GAMES_REWARD = 5;
 
-const WITHDRAW_CHUNK = 0.1;
+const WITHDRAW_CHUNK = 50;
 
 /* --- Adsgram блоки --- */
 const ADSGRAM_BLOCK_ID_TASK_MINUTE = "int-13961";
